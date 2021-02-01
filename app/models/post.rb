19c7645dc79e998c0895492f498c050bd731cbb3
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-02-01 18:26:16 +0200

    refactor post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@
 
 class Post < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :title, presence: true
   validates :description, presence: true
 end
