8313fa7f0b7bff32f9998db5805777f6baa00913
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-02-01 12:27:33 +0200

    fix rubocop errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@
 
 class Post < ApplicationRecord
   belongs_to :user
-  validates :title, length: { maximum: 100 } presence: true
+  validates :name, presence: true, length: { maximum: 50 }
   validates :description, presence: true
 end
